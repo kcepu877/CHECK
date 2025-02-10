088ca6d3630fbaab010f1f24037824bb09cb0ac3
kcepu877 <kcepu877@gmail.com>
2025-02-11 02:56:11 +0700

    Update _worker.js

--- js/_worker.js
+++ js/_worker.js
@@ -882,12 +882,20 @@ async function handleWebRequest(request) {
 
 
     const generateUUIDv4 = () => {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        const r = (Math.random() * 16) | 0;
-        const v = c === 'x' ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-      });
-    };
+  const bytes = new Uint8Array(16);
+  crypto.getRandomValues(bytes);
+
+  // Atur versi (UUID v4)
+  bytes[6] = (bytes[6] & 0x0f) | 0x40;
+  bytes[8] = (bytes[8] & 0x3f) | 0x80;
+
+  return [...bytes]
+    .map((b, i) => (i === 4 || i === 6 || i === 8 || i === 10 ? '-' : '') + b.toString(16).padStart(2, '0'))
+    .join('');
+};
+
+console.log(generateUUIDv4());
+
 
 function buildCountryFlag() {
   const flagList = cachedProxyList.map((proxy) => proxy.country);
